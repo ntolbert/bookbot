cc0b73dea9b700c99962324cdf76c64d53400b33
ntolbert <racam9w@gmail.com>
2025-07-09 08:16:26 -0400

    bookbot ch2 l6, oh hold

---
+++ stats.py
@@ -0,0 +1,8 @@
+def word_count(result):
+    s = result.split()
+    c = len(s)
+    return c
+
+def letter_count(result):
+    s = result.lower().split()
+    # going to learn dictionarys in python 1
