f746e506e9d1cd97484b88f14829eadbc94783bf
ntolbert <racam9W@gmail.com>
2025-11-26 09:13:06 -0500

    start repot func

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from stats import letter_count, word_count
+from stats import letter_count, word_count,sort_char_count 
+
 def get_book_text(filepath):
         with open(filepath) as f:
             booktext = f.read()
             return booktext
 def main():
     result = get_book_text("books/frankenstein.txt")
-   # print(result)
     num_words = word_count(result)
     print(f"{num_words} words found in the document")
-    l = letter_count(result)
-    print(l)
+    num_letters = letter_count(result)
+    #print(num_letters)
+    report = sort_char_count(num_letters)
+    #print(report)
 main()
--- stats.py
+++ stats.py
@@ -8,12 +8,25 @@ def word_count(result):
     return c
 
 def letter_count(result):
-    chars ={}
+    dict_of_chars ={}
     for letter in result:
-        s = letter.lower()
-        if s in chars:
-            chars[s]+=1 
+        current_letter = letter.lower()
+        if current_letter in dict_of_chars:
+            dict_of_chars[current_letter]+=1 
         else:
-            chars[s] = 1
-    return chars
+            dict_of_chars[current_letter] = 1
+    return dict_of_chars
+
+def dict_formatter(input_dict,new_dict_name,key_name):
+    new_dict_name = []
+    for key in input_dict:
+        new_dict_name[key_name]= key
+        print(new_dict_name)
+dict_formatter(letter_count,"test_dict","word")
+
+
+def sort_char_count(dict_of_char):
+    for char in dict_of_char:
+        print(dict_of_char)
+    return dict_of_char
 
